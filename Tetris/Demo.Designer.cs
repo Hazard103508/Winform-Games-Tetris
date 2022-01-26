17903f8d1a954b2d5181edd6e119026d736c92b6
Agustin Rosso <60651699+Hazard103508@users.noreply.github.com>
2022-01-25 22:38:28 -0300

    .
    .

---
+++ Tetris/Demo.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Tetris
+{
+    partial class Demo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Demo));
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblLines = new System.Windows.Forms.Label();
+            this.btnImage = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Canvas
+            // 
+            this.Canvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Canvas.Dock = System.Windows.Forms.DockStyle.Left;
+            this.Canvas.Image = ((System.Drawing.Image)(resources.GetObject("Canvas.Image")));
+            this.Canvas.Location = new System.Drawing.Point(0, 0);
+            this.Canvas.Size = new System.Drawing.Size(400, 650);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(473, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Lines";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.lblLines);
+            this.panel1.Location = new System.Drawing.Point(411, 37);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(121, 30);
+            this.panel1.TabIndex = 9;
+            // 
+            // lblLines
+            // 
+            this.lblLines.BackColor = System.Drawing.Color.Transparent;
+            this.lblLines.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLines.Location = new System.Drawing.Point(3, 2);
+            this.lblLines.Name = "lblLines";
+            this.lblLines.Size = new System.Drawing.Size(113, 25);
+            this.lblLines.TabIndex = 4;
+            this.lblLines.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnImage
+            // 
+            this.btnImage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.btnImage.Image = global::Tetris.Properties.Resources.Linkedin;
+            this.btnImage.Location = new System.Drawing.Point(411, 9);
+            this.btnImage.Name = "btnImage";
+            this.btnImage.Size = new System.Drawing.Size(23, 23);
+            this.btnImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.btnImage.TabIndex = 11;
+            this.btnImage.TabStop = false;
+            this.btnImage.Click += new System.EventHandler(this.btnLink_Click);
+            // 
+            // Demo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(539, 650);
+            this.Controls.Add(this.btnImage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Demo";
+            this.Text = "Tetris";
+            this.Controls.SetChildIndex(this.Canvas, 0);
+            this.Controls.SetChildIndex(this.panel1, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.btnImage, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.Canvas)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblLines;
+        private System.Windows.Forms.PictureBox btnImage;
+    }
+}
+
